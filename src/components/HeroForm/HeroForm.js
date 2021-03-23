c88caf5e891e1bf53273c97fb27f7fbcc5d853be
Car <cararr@tlen.pl>
2021-03-23 18:52:16 +0100

    refactored hero and world  forms to function components

--- src/components/HeroForm/HeroForm.js
+++ src/components/HeroForm/HeroForm.js
@@ -1,142 +1,132 @@
-import React, { Component } from 'react';
-import DataEditorIcons from '../DataEditorIcons/DataEditorIcons';
+import React, { useState, useEffect } from 'react';
 import './HeroForm.css';
+import DataEditorIcons from '../DataEditorIcons/DataEditorIcons';
 import PropTypes from 'prop-types';
 
-export default class HeroForm extends Component {
-	constructor(props) {
-		super(props);
+export default function HeroForm(props) {
+	const [currentForm, setCurrentForm] = useState({
+		name: props.selectedHero?.name || '',
+		world_id: props.selectedHero?.worldId || '',
+		type: props.selectedHero?.type || '',
+		power_level: props.selectedHero?.power || '',
+		strengths: props.selectedHero?.strengths || '',
+		weakness: props.selectedHero?.weakness || '',
+		image_url: props.selectedHero?.image || '',
+		arena_avatar_url: props.selectedHero?.avatarImage || '',
+	});
 
-		this.state = {
-			currentForm: {
-				name: this.props.selectedHero?.name || '',
-				world_id: this.props.selectedHero?.worldId || '',
-				type: this.props.selectedHero?.type || '',
-				power_level: this.props.selectedHero?.power || '',
-				strengths: this.props.selectedHero?.strengths || '',
-				weakness: this.props.selectedHero?.weakness || '',
-				image_url: this.props.selectedHero?.image || '',
-				arena_avatar_url: this.props.selectedHero?.avatarImage || '',
-			},
-		};
-	}
+	useEffect(() => {
+		setCurrentForm(() => ({
+			name: props.selectedHero?.name || '',
+			world_id: props.selectedHero?.worldId || '',
+			type: props.selectedHero?.type || '',
+			power_level: props.selectedHero?.power || '',
+			strengths: props.selectedHero?.strengths || '',
+			weakness: props.selectedHero?.weakness || '',
+			image_url: props.selectedHero?.image || '',
+			arena_avatar_url: props.selectedHero?.avatarImage || '',
+		}));
+	}, [props.selectedHero]);
 
-	changeValue = (e) => {
-		const currentForm = this.state.currentForm;
-		currentForm[e.target.name] =
-			e.target.name === 'world_id' || e.target.name === 'power_level'
-				? Number(e.target.value)
-				: e.target.value;
-		this.setState({ currentForm });
+	const changeValue = ({ target }) => {
+		const { name, value } = target;
+		setCurrentForm((prev) => ({
+			...prev,
+			[name]:
+				name === 'world_id' || name === 'power_level' ? Number(value) : value,
+		}));
 	};
 
-	render() {
-		const worldsRadios = this.props.worlds.map((world) => (
-			<label key={world.name} htmlFor={world.id}>
-				<input
-					onChange={this.changeValue}
-					checked={this.state.currentForm.world_id === world.id}
-					name="world_id"
-					id={world.id}
-					type="radio"
-					value={world.id}
-				></input>
-				{world.id}
-			</label>
-		));
-		return (
-			<div>
-				<form>
-					<label>Name:</label>
-					<input
-						required
-						name="name"
-						value={this.state.currentForm.name}
-						type="text"
-						onChange={this.changeValue}
-					></input>
-					<label>World:</label>
-					<div className="hero-form-worlds-radio">{worldsRadios}</div>
-					<label>Type:</label>
-					<input
-						maxLength={25}
-						name="type"
-						value={this.state.currentForm.type}
-						type="text"
-						onChange={this.changeValue}
-					></input>
-					<label>Power:</label>
-					<input
-						required
-						name="power_level"
-						value={this.state.currentForm.power_level}
-						type="number"
-						min="1"
-						max="100"
-						onChange={this.changeValue}
-					></input>
-					<label>Strenghts:</label>
-					<input
-						name="strengths"
-						value={this.state.currentForm.strengths}
-						type="text"
-						onChange={this.changeValue}
-					></input>
-					<label>Weakness:</label>
-					<input
-						name="weakness"
-						value={this.state.currentForm.weakness}
-						type="text"
-						onChange={this.changeValue}
-					></input>
-					<label>Image:</label>
-					<textarea
-						required
-						name="image_url"
-						rows={5}
-						value={this.state.currentForm.image_url}
-						// @ts-ignore
-						type="text"
-						onChange={this.changeValue}
-					></textarea>
-					<label>Avatar:</label>
-					<textarea
-						required
-						name="arena_avatar_url"
-						rows={5}
-						value={this.state.currentForm.arena_avatar_url}
-						// @ts-ignore
-						type="text"
-						onChange={this.changeValue}
-					></textarea>
-				</form>
-				<DataEditorIcons
-					isCreateHeroOn={this.props.isCreateHeroOn}
-					currentForm={this.state.currentForm}
-					selectedHero={this.props.selectedHero}
-					addInstance={this.props.addInstance}
-					updateInstance={this.props.updateInstance}
-					deleteInstance={this.props.deleteInstance}
-				/>
-			</div>
-		);
-	}
+	const worldsRadios = props.worlds.map((world) => (
+		<label key={world.name} htmlFor={world.id}>
+			<input
+				onChange={changeValue}
+				checked={currentForm.world_id === world.id}
+				name="world_id"
+				id={world.id}
+				type="radio"
+				value={world.id}
+			></input>
+			{world.id}
+		</label>
+	));
 
-	componentDidUpdate(prevProps) {
-		if (this.props.selectedHero !== prevProps.selectedHero)
-			this.setState({
-				currentForm: {
-					name: this.props.selectedHero.name || '',
-					world_id: this.props.selectedHero.worldId,
-					type: this.props.selectedHero.type || '',
-					power_level: this.props.selectedHero.power,
-					strengths: this.props.selectedHero.strengths || '',
-					weakness: this.props.selectedHero.weakness || '',
-					image_url: this.props.selectedHero.image || '',
-					arena_avatar_url: this.props.selectedHero.avatarImage || '',
-				},
-			});
-	}
+	return (
+		<div>
+			<form>
+				<label>Name:</label>
+				<input
+					required
+					name="name"
+					value={currentForm.name}
+					type="text"
+					onChange={changeValue}
+				></input>
+				<label>World:</label>
+				<div className="hero-form-worlds-radio">{worldsRadios}</div>
+				<label>Type:</label>
+				<input
+					maxLength={25}
+					name="type"
+					value={currentForm.type}
+					type="text"
+					onChange={changeValue}
+				></input>
+				<label>Power:</label>
+				<input
+					required
+					name="power_level"
+					value={currentForm.power_level}
+					type="number"
+					min="1"
+					max="100"
+					onChange={changeValue}
+				></input>
+				<label>Strenghts:</label>
+				<input
+					name="strengths"
+					value={currentForm.strengths}
+					type="text"
+					onChange={changeValue}
+				></input>
+				<label>Weakness:</label>
+				<input
+					name="weakness"
+					value={currentForm.weakness}
+					type="text"
+					onChange={changeValue}
+				></input>
+				<label>Image:</label>
+				<textarea
+					required
+					name="image_url"
+					rows={5}
+					value={currentForm.image_url}
+					// @ts-ignore
+					type="text"
+					onChange={changeValue}
+				></textarea>
+				<label>Avatar:</label>
+				<textarea
+					required
+					name="arena_avatar_url"
+					rows={5}
+					value={currentForm.arena_avatar_url}
+					// @ts-ignore
+					type="text"
+					onChange={changeValue}
+				></textarea>
+			</form>
+			<DataEditorIcons
+				isCreateHeroOn={props.isCreateHeroOn}
+				currentForm={currentForm}
+				selectedHero={props.selectedHero}
+				addInstance={props.addInstance}
+				updateInstance={props.updateInstance}
+				deleteInstance={props.deleteInstance}
+			/>
+		</div>
+	);
 }
 
 HeroForm.propTypes = {
--- src/components/WorldForm/WorldForm.js
+++ src/components/WorldForm/WorldForm.js
@@ -1,82 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './WorldForm.css';
 import DataEditorIcons from '../DataEditorIcons/DataEditorIcons';
 import PropTypes from 'prop-types';
 
-export default class WorldForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentForm: {
-				name: this.props.selectedWorld?.name || '',
-				world_origin: this.props.selectedWorld?.worldOrigin || '',
-				logo_url: this.props.selectedWorld?.logoUrl || '',
-			},
-		};
-	}
+export default function WorldForm(props) {
+	const [currentForm, setCurrentForm] = useState({
+		name: props.selectedWorld?.name || '',
+		world_origin: props.selectedWorld?.worldOrigin || '',
+		logo_url: props.selectedWorld?.logoUrl || '',
+	});
 
-	changeValue = (e) => {
-		const currentForm = this.state.currentForm;
-		currentForm[e.target.name] = e.target.value;
-		this.setState({ currentForm });
+	useEffect(() => {
+		setCurrentForm(() => ({
+			name: props.selectedWorld?.name || '',
+			world_origin: props.selectedWorld?.worldOrigin || '',
+			logo_url: props.selectedWorld?.logoUrl || '',
+		}));
+	}, [props.selectedWorld]);
+
+	const changeValue = ({ target }) => {
+		const { name, value } = target;
+		setCurrentForm((prev) => ({ ...prev, [name]: value }));
 	};
 
-	render() {
-		return (
-			<div>
-				<form>
-					<label>Name:</label>
-					<input
-						maxLength={25}
-						required
-						name="name"
-						onChange={this.changeValue}
-						value={this.state.currentForm.name}
-						type="text"
-					></input>
-					<label>Origin:</label>
-					<input
-						maxLength={100}
-						name="world_origin"
-						onChange={this.changeValue}
-						value={this.state.currentForm.world_origin}
-						type="text"
-					></input>
-					<label>Logo:</label>
-					<textarea
-						rows={5}
-						name="logo_url"
-						onChange={this.changeValue}
-						value={this.state.currentForm.logo_url}
-						//@ts-ignore
-						type="text"
-					></textarea>
-				</form>
-				{
-					<DataEditorIcons
-						isCreateWorldOn={this.props.isCreateWorldOn}
-						currentForm={this.state.currentForm}
-						selectedWorld={this.props.selectedWorld}
-						addInstance={this.props.addInstance}
-						updateInstance={this.props.updateInstance}
-						deleteInstance={this.props.deleteInstance}
-					/>
-				}
-			</div>
-		);
-	}
-
-	componentDidUpdate(prevProps) {
-		if (this.props.selectedWorld !== prevProps.selectedWorld) {
-			this.setState({
-				currentForm: {
-					name: this.props.selectedWorld.name,
-					world_origin: this.props.selectedWorld.worldOrigin || '',
-					logo_url: this.props.selectedWorld.logoUrl || '',
-				},
-			});
-		}
-	}
+	return (
+		<div>
+			<form>
+				<label>Name:</label>
+				<input
+					maxLength={25}
+					required
+					name="name"
+					onChange={changeValue}
+					value={currentForm.name}
+					type="text"
+				></input>
+				<label>Origin:</label>
+				<input
+					maxLength={100}
+					name="world_origin"
+					onChange={changeValue}
+					value={currentForm.world_origin}
+					type="text"
+				></input>
+				<label>Logo:</label>
+				<textarea
+					rows={5}
+					name="logo_url"
+					onChange={changeValue}
+					value={currentForm.logo_url}
+					//@ts-ignore
+					type="text"
+				></textarea>
+			</form>
+			{
+				<DataEditorIcons
+					isCreateWorldOn={props.isCreateWorldOn}
+					currentForm={currentForm}
+					selectedWorld={props.selectedWorld}
+					addInstance={props.addInstance}
+					updateInstance={props.updateInstance}
+					deleteInstance={props.deleteInstance}
+				/>
+			}
+		</div>
+	);
 }
 
 WorldForm.propTypes = {
